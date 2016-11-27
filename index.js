3e7dc111ba0e5e997892afd691e9f9be2b040fcf
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-11-27 12:01:24 +0100

    challenge and login

--- index.js
+++ index.js
@@ -17,10 +17,19 @@ var transporter = nodemailer.createTransport(
  'smtps://nycdaamswdi%40gmail.com:'+
  process.env.EMAIL_PASSWORD_Blog_App+'@smtp.gmail.com');
 
- app.get('/registration', (req, res) => {
+ app.get('/register', (req, res) => {
     res.render('users/new');
  });
+app.get('/login', (req, res) => {
+  res.redirect('/admin');
+});
 
+app.get('/admin', (req, res) => {
+  res.render('users/login');
+});
+app.get('/admin/challenge', (req, res) => {
+  res.render('challenge/new');
+});
 
  app.listen(3000, () => {
     console.log('Web Server is running on port 3000');
