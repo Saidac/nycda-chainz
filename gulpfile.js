eb6508210d6f950340a17dc6e4b5cbd3a907aa4d
William Saidac <sdfsfsds>
2016-12-02 10:49:58 +0100

    fixed asset folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', () => {
+  console.log('default task runs');
 });
